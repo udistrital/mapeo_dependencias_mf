72b8ca17c123f75def809a9915198d8e97dd0c6d
Skyrus1305 <dagorin7@gmail.com>
2024-09-11 18:33:23 -0500

    Se ajusta el fornt según mockups y se comienzan a implementar endpoints, sin embargo fallan ya que se recibe como respuesta xml

--- src/app/managers/requestManager.ts
+++ src/app/managers/requestManager.ts
@@ -21,11 +21,14 @@ export class RequestManager {
     const acces_token = window.localStorage.getItem('access_token');
     if (acces_token !== null) {
       this.httpOptions = {
-         headers: new HttpHeaders({
-           'Content-Type': 'application/json',
-           'Authorization': `Bearer ${acces_token}`,
-         }),
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          'Accept': 'application/json, application/xml, text/plain',
+          'Authorization': `Bearer ${acces_token}`,
+        }),
       }
+      const headers = this.httpOptions.headers.keys().map((key:string) => ({ [key]: this.httpOptions.headers.get(key) }));
+      console.log("CREACION HTTP HEADER:", headers);
     }
   }
 
@@ -45,7 +48,10 @@ export class RequestManager {
    * @param params (an Key, Value object with que query params for the request)
    * @returns Observable<any>
    */
+  /*
   get(endpoint: any) {
+    const headers = this.httpOptions.headers.keys().map((key:string) => ({ [key]: this.httpOptions.headers.get(key) }));
+    console.log("HTTP HEADER:", headers);
     return this.http.get<any>(`${this.path}${endpoint}`, this.httpOptions).pipe(
       map(
         (res) => {
@@ -58,8 +64,73 @@ export class RequestManager {
       ),
       catchError(this.errManager.handleError.bind(this)),
     );
-  }
+  }*/
+  //INICIO PRUEBA RECEPCIÓN RESPUESTAS DInÄMICAS
+    get(endpoint: any) {
+      return this.http.get(`${this.path}${endpoint}`, { ...this.httpOptions, responseType: 'arraybuffer' }).pipe(
+        map((response: ArrayBuffer) => {
+          
+          const text = new TextDecoder().decode(response);
+          if (this.isXML(text)) {
+            return this.parseXML(text);  
+          } else {
+            return JSON.parse(text); 
+          }
+        }),
+        catchError(error => {
+          console.error('Error en la solicitud:', error);
+          return this.errManager.handleError(error); 
+        }),
+      );
+    }
+  
 
+    private isXML(response: string): boolean {
+      return response.startsWith('<') && response.includes('<?xml');
+    }
+
+    private parseXML(xmlString: string) {
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(xmlString, 'application/xml');
+      return this.xmlToJson(xml);
+    }
+  
+    
+    private xmlToJson(xml: any): any {
+      let obj: any = {};
+  
+      if (xml.nodeType === 1) {
+        if (xml.attributes.length > 0) {
+          obj["@attributes"] = {};
+          for (let j = 0; j < xml.attributes.length; j++) {
+            const attribute = xml.attributes.item(j);
+            obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+          }
+        }
+      } else if (xml.nodeType === 3) {
+        obj = xml.nodeValue;
+      }
+  
+      if (xml.hasChildNodes()) {
+        for (let i = 0; i < xml.childNodes.length; i++) {
+          const item = xml.childNodes.item(i);
+          const nodeName = item.nodeName;
+          if (typeof obj[nodeName] === "undefined") {
+            obj[nodeName] = this.xmlToJson(item);
+          } else {
+            if (typeof obj[nodeName].push === "undefined") {
+              const old = obj[nodeName];
+              obj[nodeName] = [];
+              obj[nodeName].push(old);
+            }
+            obj[nodeName].push(this.xmlToJson(item));
+          }
+        }
+      }
+  
+      return obj;
+    }
+// FIN PRUEBA DE RECEPCION DINAMICA DE RESPUESTAS
   /**
    * Perform a POST http request
    * @param endpoint service's end-point
--- src/app/models/mapeo-busqueda.models.ts
+++ src/app/models/mapeo-busqueda.models.ts
@@ -4,4 +4,8 @@ export interface MapeoBusqueda {
     dependenciasAsociadas: string;
     tipoDependencia: string[];
     tipo: string;
+    idArgo: number;
+    numIdInterno: number;
+    codSnies: number;
+    codIris: number; 
 }
