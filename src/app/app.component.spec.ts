4ceccb0359188e03b5b2360781c2bf3646769b1b
Diego Gutierrez <diagutierrezro@unal.edu.co>
2024-09-26 12:22:13 -0500

    fix: Ajustes para prueba en despliegue

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mapeo'`, () => {
+  it(`should have as title 'mapeo-dependencias-mf'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mapeo');
+    expect(app.title).toEqual('mapeo-dependencias-mf');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('mapeo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('mapeo-dependencias-mf app is running!');
   });
 });
